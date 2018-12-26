eb01d9c3500e8db4db9fc3284c3e1c96966bd5f6
liuhao <json_liuhao@163.com>
2018-12-27 01:14:47 +0800

    常用系统函数完成

--- basic/loop/loop.go
+++ basic/loop/loop.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
+	"strings"
 )
 
 //将整数转换为2进制
@@ -30,7 +32,17 @@ func printFile(fileName string) {
 	if err != nil {
 		panic(err)
 	}
-	scanner := bufio.NewScanner(file)
+	printFileContents(file)
+}
+
+//go语言中的Reader和Writer不仅可以操作文件,网络还有string,byte,slice 都是作为Reader和Writer
+//我们在点开os.Open的时候会发现返回的是一个file的结构体里面就有一个Read()就是对Read()接口的一个实现
+//而且我们发现bufio.NewScanner()里面要的就是一个Reader,通常情况下我们不用file,而是什么都用Reader和Writer
+//Reader和Write更加关键的地方是,fmt.Fprintf(),fmt.Fscanf()里面的第一个参数就是一个reader,这时候我们给网络连接啊什么的都是可以用的
+//针对Reader和Writer各写一个接口之后,可以装在文件上也可以装在网络上.非常的方便
+//因此我们在比较底层的读写相关的东西都希望把他做成Reader和Writer,这样我们就可以和许多系统的函数一起共用
+func printFileContents(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
 	//go 语言没有while,这个就是while
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
@@ -54,5 +66,14 @@ func main() {
 	)
 	//读取文件内容,输出
 	printFile("basic/loop/aaa.log");
-	forever();
+	//在go语言中反引号代表跨行的字符串
+	s := `abc"d"
+kkkk
+123
+
+pppp`
+	//像文件一样打印字符串,除了strings.NewReader之外还有bytes.NewReader两个差不多只是用的类型不一样
+	printFileContents(strings.NewReader(s))
+
+	//forever();
 }
