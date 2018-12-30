eacf1e4950e7c9f9583d45df746286993c1a276a
liuhao <json_liuhao@163.com>
2018-12-31 04:43:16 +0800

    服务器统一出错处理2

--- errhandling/filelistingserver/filelisting/handler.go
+++ errhandling/filelistingserver/filelisting/handler.go
@@ -4,11 +4,33 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
+const prefix = "/list/"
+
+//这里因为我们要用到userError,所以这里我们也定义一个userError
+//go语言的接口,两边是各自管各自的定义的,那边定义的interface只是说他要实现一个error和message()
+//但是对这边是不影响的,如果这边要用就要实现自己的userError
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+
+func (e userError) Message() string {
+	return string(e)
+}
+
 func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
+	//如果不存在/list/提示错误,避免内存越界
+	if strings.Index(request.URL.Path, prefix) != 0 {
+		return userError("path must start with" + prefix)
+		//return errors.New("path must start with" + prefix)
+	}
+
 	//获得请求的URL地址,通过len()计算出/list/的长度,然后进行剪切得到URI文件地址
-	path := request.URL.Path[len("/list/"):]
+	path := request.URL.Path[len(prefix):]
 	//打开文件
 	file, err := os.Open(path)
 	if err != nil {
--- errhandling/filelistingserver/web.go
+++ errhandling/filelistingserver/web.go
@@ -19,10 +19,28 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 
 	return func(writer http.ResponseWriter, request *http.Request) {
+		//定义一个延时执行的函数,捕捉错误信息
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
 		err := handler(writer, request)
 		if err != nil {
 			//将具体的系统的错误信息在命令行中输出
 			log.Printf("Error ocurred"+"handling request: %s", err.Error())
+
+			//如果错误信息假定为userError类型,给用户展示用户的错误信息
+			if userErr, ok := err.(userError); ok {
+				http.Error(
+					writer,
+					userErr.Message(),
+					http.StatusBadRequest,
+				)
+				return
+			}
+
 			//匹配错误信息,将不必要的信息过滤,不展示给前台看
 			code := http.StatusOK
 			switch {
@@ -43,10 +61,21 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	}
 }
 
+//给用户看的错误信息
+//go语言的接口,两边是各自管各自的定义的,这边定义的userError只是说我要实现一个error和message()
+//然后那边用的人呢,自己去管实现自己的userError,就能给外面的人用得到,这两个接口的本身是不需要相互看得到的
+type userError interface {
+	error            //给系统看的错误信息
+	Message() string //给用户看的错误信息
+}
+
 func main() {
 
 	//设置文件访问的路径, 原本第二个参数是一个回调函数, 我们之前直接把回调函数写在后面的, 现在剥离出去了, 因为业务部分写在这里不合适
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileList))
+	//这里我们把/list/改成"/",filelisting.HandleFileList保持不变, 访问的时候就会报错,此时访问时,里面的函数,就是提示slice越界
+	//因为我们访问的时候不加/list/直接用文件名,他会URL进行剪切了/list/然后对剩下的字符串进行匹配,在用改字符串访问文件,现在的slice肯定不够长了
+	//虽然越界导致错误,但是这个httpserver没有挂, 因为httpserver做了保护,使用recover做的保护,但是还会panic()报错,我们不想报错,所以在上面使用recover判断加上了保护
+	http.HandleFunc("/", errWrapper(filelisting.HandleFileList))
 
 	//开启监听服务器
 	err := http.ListenAndServe(":8881", nil)
