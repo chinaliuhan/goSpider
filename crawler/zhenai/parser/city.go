435c5f1201182b754c760d77bc081dd794b82042
liuhao <json_liuhao@163.com>
2019-01-19 17:14:31 +0800

    城市解析器完成

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -5,12 +5,12 @@ import (
 	"regexp"
 )
 
-const cityListRe = `<a\s+href="(http://www.zhenai.com/zhenghun/[0-9a-zA-Z]+)"[^>]*>([^<]+)</a>`
+const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
 
-//城市列表解析器,接收一段HTML,匹配URL列表,返回一个拼装好的解析实例
-func ParseCityList(contents []byte) engine.ParseResult {
+//城市解析器,将城市的URL地址传进来,获取用户的URL地址
+func ParseCity(contents []byte) engine.ParseResult {
 	//生成正则表达式,一般我们自己写的用MustCompile,否则用Compile()处理错误信息
-	re := regexp.MustCompile(cityListRe)
+	re := regexp.MustCompile(cityRe)
 	//返回一个[]byte ,相当于是一组被匹配到的字符串
 	//matches := re.FindAll(contents, -1)
 	//子匹配
@@ -19,11 +19,11 @@ func ParseCityList(contents []byte) engine.ParseResult {
 	result := engine.ParseResult{}
 	for _, m := range matches {
 		//向实例中最追加城市名称
-		result.Items = append(result.Items, "City "+ string(m[2]))
+		result.Items = append(result.Items, "User "+string(m[2]))
 		//请求实例中追加URL地址,同时拼装请求实例,追加到解析实例中的请求属性中
 		result.Requests = append(result.Requests, engine.Request{
 			Url:        string(m[1]),
-			ParserFunc: ParseCity,
+			ParserFunc: engine.NilParser,
 		})
 	}
 
